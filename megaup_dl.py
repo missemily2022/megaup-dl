fcaf9a1010c7789847badafa71e5f327d5d069d6
Miss Emily <missemilymirror@gmail.com>
2022-06-05 19:47:27 +0530

    Typos

--- megaup_dl.py
+++ megaup_dl.py
@@ -11,7 +11,7 @@ try:
 except:
     print("Some error occurred while generating direct link. It may be possible that Link is Invalid. Check once again.")
 data = [a.strip("' ") for a in data]
-time.sleep(3)
+sleep(3)
 idurl = ""
 for i in range(int(len(data[0])/4) - 1, -1, -1):
     idurl += data[0][i]
@@ -21,4 +21,4 @@ for i in range(int((len(data[1]) - 3) / 2 + 2), 2, -1):
     idurl += data[1][i]
 des_url = f"https://download.megaup.net/?idurl={idurl}&idfilename={data[2]}&idfilesize={data[3]}"
 des_url = des_url.replace(' ', '%20')
-print("Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ: ",des_url)
+print("Download Link : ",des_url)
